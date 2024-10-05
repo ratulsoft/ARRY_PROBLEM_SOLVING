2f3874958c66b326845a03e2747fdddf60718bcc
ratulsoft <mdratulbd1971@gmail.com>
2024-10-05 22:04:33 +0600

    first commit

---
+++ problem_1.js
@@ -0,0 +1,16 @@
+//1. Sum of All Elements: Write a function that returns the sum of all elements in an array.
+//let's begin
+
+
+//echo "# gffg" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/ratulsoft/gffg.git
+//git push -u origin main
+
+
+///git remote add origin https://github.com/ratulsoft/gffg.git
+//'git branch -M main
+//https://github.com/ratulsoft/ARRY-prgit push -u origin main
