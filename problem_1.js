91989d8f60c14e93b50a70071bfd4824cceb47f6
ratulsoft <mdratulbd1971@gmail.com>
2024-10-05 22:07:36 +0600

    second commit

--- problem_1.js
+++ problem_1.js
@@ -2,15 +2,4 @@
 //let's begin
 
 
-//echo "# gffg" >> README.md
-//git init
-//git add README.md
-//git commit -m "first commit"
-//git branch -M main
-//git remote add origin https://github.com/ratulsoft/gffg.git
-//git push -u origin main
-
-
-///git remote add origin https://github.com/ratulsoft/gffg.git
-//'git branch -M main
-//https://github.com/ratulsoft/ARRY-prgit push -u origin main
+console.log("hello world");
